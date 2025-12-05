ee6abbcb20e5b606b8f9142f215d068ab396d9ca
21471a0593 <166141416+21471a0593@users.noreply.github.com>
2025-12-05 22:34:17 +0530

    Update task-2.py

--- task-2.py
+++ task-2.py
@@ -1,1 +1,16 @@
+1.  Question 
+class Solution:
+    def runningSum(self, nums):
+        result = []
+        total = 0
+        
+        for n in nums:
+            total += n
+            result.append(total)
+        
+        return result
+      obj = Solution()
+nums = [1, 2, 3, 4]
+output = obj.runningSum(nums)
+print(output)
 
